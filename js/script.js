837d3dfc612c45598b00baba9138fac7f8f52f80
SiimonnLeGalerien <SiimonnLeGalerien@github.com>
2022-06-25 20:17:53 +0200

    const pour les querySelector

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var counter = 420;
-var element = document.querySelector('#nombre');
+const element = document.querySelector('#nombre');
 element.innerHTML = 'nombre : ' + counter;
-var plusOne = document.querySelector('#plusOne');
+const plusOne = document.querySelector('#plusOne');
 plusOne.addEventListener('click', () => {
     counter++;
     element.innerHTML = 'nombre : ' + counter;
 });
-var minusOne = document.querySelector('#minusOne');
+const minusOne = document.querySelector('#minusOne');
 minusOne.addEventListener('click', () => {
     counter--;
     element.innerHTML = 'nombre : ' + counter;
